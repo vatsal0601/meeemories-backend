aedb031fbc2cd02a14fec0f4a81111dff9890424
Vatsal Sakariya <vatsal.sakariya@toddleapp.com>
2023-06-06 17:23:49 +0530

    style(page.tsx): change background color to a radial gradient from slate-950 to slate-800 style(page.tsx): change text color to slate-50

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <main className="grid min-h-screen place-items-center">
-      <h1 className="text-6xl font-bold">Meeemories Backend</h1>
+    <main className="grid min-h-screen place-items-center bg-gradient-radial from-slate-950 to-slate-800">
+      <h1 className="text-6xl font-bold text-slate-50">Meeemories Backend</h1>
     </main>
   );
 };
