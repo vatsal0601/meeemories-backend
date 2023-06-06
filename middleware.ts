81ee4a30219e92edadd8a7e02d484f3cf9022aac
Vatsal Sakariya <vatsal.sakariya@toddleapp.com>
2023-06-06 16:08:20 +0530

    feat(middleware.ts): add debug flag to authMiddleware and afterAuth function to handle unauthorized requests

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,11 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api"],
+  debug: process.env.NODE_ENV === "development",
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute)
+      return new Response("Unauthorized", { status: 401 });
+  },
 });
 
 export const config = {
